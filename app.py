29443df9112e3b5a163270a625d74180d48e3526
Ya-Tin <mysticalyetium@gmail.com>
2025-08-26 18:48:17 +0530

    Day 1 Orientation Bug Fixes

--- app.py
+++ app.py
@@ -16,13 +16,13 @@ from langchain_google_genai import (
     ChatGoogleGenerativeAI,
 )
 import google.generativeai as genai
-import logging
+# import logging
 
-logging.basicConfig(
-    filename="chat_log.txt",
-    level=logging.INFO, 
-    format="%(asctime)s-%(message)s"
-)
+# logging.basicConfig(
+#     filename="chat_log.txt",
+#     level=logging.INFO, 
+#     format="%(asctime)s-%(message)s"
+# )
 
 # -----------------------------
 # HTML background animation
@@ -138,7 +138,7 @@ def get_conversational_chain():
         """
         You are a senior at Punjab Engineering College, you try to answer the questions as per the knowledge base provided. Try to generate response that are friendly. Avoid using "Alright", "Hey There", "So" and other filler words in the beginning of the sentence. 
         Answer in informal tone to sound friendly. Also include emojis in your response if relevent. 
-        Try using knowledge base for finding the answer, but if the answer is not available in the context, reply with "Not enough information is available in the knowledge base trained on, but I can get an answer based on the Internet knowledge." and generate a response using Internet data.
+        Try using knowledge base for finding the answer, but if the answer is not available in the context, reply with "..." and generate a response using Internet data, don't let user know that you don't have the information in your knowledge base.
         Try to strictly answer in 200 words. 
         Your Knowledge Base about PEC:
         {context}
@@ -275,7 +275,7 @@ def main():
 
     # Chat input
     user_question = st.chat_input("Input your Query here and press Enter")
-    logging.info(f"User asked: {user_question}")
+    # logging.info(f"User asked: {user_question}")
 
     if user_question:
         st.session_state["messages"].append({"role": "user", "content": user_question})
